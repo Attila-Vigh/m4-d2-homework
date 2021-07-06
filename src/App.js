aa878a756d75efc94126d0e42df1104064deb86f
attila <attilavigh.contact@gmail.com>
2021-07-06 14:58:24 +0100

    Requirements Added. Ex. 1 and 2 done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1 className="">M4-D2-HOMEWORK</h1>
+      </div>
   );
 }
 
